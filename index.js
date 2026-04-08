bdb133e07c9a1f9858b391dd253692808be96e11
nayuta <na72.lamb@gmail.com>
2026-04-08 23:32:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//文字列型の変数
+const str = "Hello World!!!!!";
+console.log(str);
+
+//数字型の変数
+const num = 1234;
+console.log(num);
+
+//真偽値の変数
+const bool = false;
+console.log(false);
+
+
+const hoge = "ほげ";
+const fuge = "ふが";
+console.log(hoge + fuge);
+
+const a = 10;
+const b = 14;
+console.log
+
+//文字列型の変数
+const sei = "";
+const mei = "ばあ";
+const moji = `${foo} : ${bar}`;
+console.log(moji);
